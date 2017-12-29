a3a988377cdc06d7bc743c4152e04a77e8c6eea2
Evgeny <evgeny@MacBook-Pro-Evgeny.local>
2017-12-30 00:25:31 +0300

    add all functionality

--- OOP/lab_3/Lab_3.js
+++ OOP/lab_3/Lab_3.js
@@ -1,33 +1,50 @@
-let ticketsWh = [];
+let OrdersWh = [];
 
-class Tickets{
+class Orders{
     constructor(destination, flightNumber, surnameAndInitials, departureDate, orderNumber){
         this.destination = destination,
         this.flightNumber = flightNumber,
         this.surnameAndInitials = surnameAndInitials,
         this.departureDate = departureDate
         // this.orderNumber = orderNumber
-    }
-
+    };
 }
 
 let addOrder = (option) =>{
     let arr = option.split(' ');
-    let newOrder = new Tickets(arr[0],arr[1],arr[2],arr[3],arr[4]);
-    ticketsWh.push(newOrder);
+    let newOrder = new Orders(arr[0],arr[1],arr[2],arr[3],arr[4]);
+    OrdersWh.push(newOrder);
 };
 
 let deleteOrder = (flightNumber, surnameAndInitials)=>{
-    for (let i = 0; i < ticketsWh.length; i++) {
-        if (ticketsWh[i].flightNumber == flightNumber && ticketsWh[i].surnameAndInitials == surnameAndInitials) {
-            ticketsWh.splice(i,i)
+    for (let i = 0; i < OrdersWh.length; i++) {
+        if (OrdersWh[i].flightNumber == flightNumber && OrdersWh[i].surnameAndInitials == surnameAndInitials) {
+            OrdersWh.splice(i,i)
         };
     };
-    console.log(ticketsWh);
 };
 
+let showOrderByNumber = (flightNumber, departureDate)=>{
+    let result = [];
+    for (let i = 0; i < OrdersWh.length; i++) {
+        if (OrdersWh[i].flightNumber == flightNumber && OrdersWh[i].departureDate == departureDate) {
+            result.push(OrdersWh[i]);
+        };
+    };
+    console.log(result)
+};
+
+let showAllOrder = ()=>{
+    console.log(OrdersWh)
+}
+
 addOrder('Moscow 271 Volkov.A.V. 2017.12.31');
+addOrder('Moscow 271 Petrov.P.P. 2017.12.31');
 addOrder('Minsk 139 Starovoitov.E.A. 2018.01.22');
 addOrder('St.Petersberg 222 Liberman.S.I. 2018.02.17');
 
 deleteOrder('222', 'Liberman.S.I.');
+
+showOrderByNumber('271', '2017.12.31');
+
+showAllOrder();
