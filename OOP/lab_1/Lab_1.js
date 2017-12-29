2a59f4dfed9fd1229a8c2e6e84313842508696d1
Evgeny <evgeny@MacBook-Pro-Evgeny.local>
2017-12-29 20:18:34 +0300

    add all functionality

--- OOP/lab_1/Lab_1.js
+++ OOP/lab_1/Lab_1.js
@@ -6,7 +6,8 @@ class Person {
         this.address = address,
         this.sex = sex,
         this.educate = educate,
-        this.birthday = birthday;
+        this.birthday = birthday,
+        this.age = nowYear - birthday
     };
     get FullName(){
         return this.surname + ' ' + this.name;
@@ -20,15 +21,59 @@ class Person {
     };
     showPerson(){
         console.log(this.surname, this.name, this.patronym, this.address, this.sex,
-        this.educate, this.birthday
+        this.educate, this.birthday, this.age
         );
     };
 };
 
-let Evgen = new Person("Starov", "evgen", "Alex", "Mog");
+let now = new Date();
+let nowYear = now.getFullYear();
 
+let arr = []
+
+let Evgen = new Person("Starov", "evgen", "Alex", "Mog"),
+    Izya = new Person("Старовойтов", "Евгений", "Александрович", "Могилев", "male", "MS", "1992"),
+    Moisha = new Person("Моисеенко", "Игорь", "Витальевич", "Минск", "male", "HS", "1988"),
+    Sara = new Person("Либерман", "Сара", "Иосифовна", "Иерусалим", "female", "HS", "1995"),
+    Lola = new Person("Блестящая", "Лола", "Олеговна", "Витебск", "female", "MS", "1972");
+
+arr.push(Evgen, Izya, Moisha, Sara, Lola);
+console.log(arr);
 Evgen.showPerson();
 
-Evgen.allDataPerson = 'David Yosa Rabinovich Tel-Aviv male HS 17.11.91';
+Evgen.allDataPerson = 'David Yosa Rabinovich Tel-Aviv male HS 1991';
+
+Izya.showPerson();
 
 Evgen.FullName;
+
+let sortByAge = (age, array) => {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].age >= age ) {
+            result.push(array[i])
+        }
+    }
+    console.log(result)
+};
+let sortByEducate = (educate, array) => {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].educate == educate ) {
+            result.push(array[i])
+        }
+    }
+    console.log(result)
+};
+let sortBySex = (sex, array) => {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].sex >= sex ) {
+            result.push(array[i])
+        }
+    }
+    console.log(result)
+}
+sortByAge("26", arr );
+sortByEducate("HS", arr );
+sortBySex("male", arr );
