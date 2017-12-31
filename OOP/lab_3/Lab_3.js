6863e674e1d8a798f8e4d4a74a616d2f0b5bcaeb
Evgeny <evgeny@MacBook-Pro-Evgeny.local>
2017-12-31 20:39:42 +0300

    remake a lab_3

--- OOP/lab_3/Lab_3.js
+++ OOP/lab_3/Lab_3.js
@@ -1,50 +1,51 @@
-let OrdersWh = [];
+class List{
+    constructor(){
+        this.warehouse = [];
+    };
+    addElem(elem){
+        this.warehouse.push(elem);
+    };
+    removeElem(flightNumber, surnameAndInitials){
+        for (let i = 0; i < this.warehouse.length; i++) {
+            if (this.warehouse[i].flightNumber == flightNumber && this.warehouse[i].surnameAndInitials == surnameAndInitials) {
+                this.warehouse.splice(i,i)
+            };
+        };
+    };
+    showElementByNumber(flightNumber, departureDate){
+        let result = [];
+        for (let i = 0; i < this.warehouse.length; i++) {
+            if (this.warehouse[i].flightNumber == flightNumber && this.warehouse[i].departureDate == departureDate) {
+                result.push(this.warehouse[i]);
+            };
+        };
+        console.log(result);
+    };
+    showAllElements(){
+        console.log(this.warehouse);
+    };
+}
 
-class Orders{
+class Order{
     constructor(destination, flightNumber, surnameAndInitials, departureDate, orderNumber){
         this.destination = destination,
         this.flightNumber = flightNumber,
         this.surnameAndInitials = surnameAndInitials,
         this.departureDate = departureDate
-        // this.orderNumber = orderNumber
-    };
-}
-
-let addOrder = (option) =>{
-    let arr = option.split(' ');
-    let newOrder = new Orders(arr[0],arr[1],arr[2],arr[3],arr[4]);
-    OrdersWh.push(newOrder);
-};
-
-let deleteOrder = (flightNumber, surnameAndInitials)=>{
-    for (let i = 0; i < OrdersWh.length; i++) {
-        if (OrdersWh[i].flightNumber == flightNumber && OrdersWh[i].surnameAndInitials == surnameAndInitials) {
-            OrdersWh.splice(i,i)
-        };
     };
 };
 
-let showOrderByNumber = (flightNumber, departureDate)=>{
-    let result = [];
-    for (let i = 0; i < OrdersWh.length; i++) {
-        if (OrdersWh[i].flightNumber == flightNumber && OrdersWh[i].departureDate == departureDate) {
-            result.push(OrdersWh[i]);
-        };
-    };
-    console.log(result)
-};
+let wh = new List();
 
-let showAllOrder = ()=>{
-    console.log(OrdersWh)
-}
+wh.addElem(new Order('Moscow', '271', 'Volkov.A.V.', '2017.12.31'));
+wh.addElem(new Order('Miami', '269', 'Rabinovich.A.A.', '2018.03.21'));
+wh.addElem(new Order('Moscow', '271', 'Petrov.P.P.', '2017.12.31'));
+wh.addElem(new Order('Minsk', '139', 'Starovoitov.E.A.', '2018.01.22'));
+wh.addElem(new Order('St.Petersberg', '222', 'Liberman.S.I.', '2018.02.17'));
 
-addOrder('Moscow 271 Volkov.A.V. 2017.12.31');
-addOrder('Moscow 271 Petrov.P.P. 2017.12.31');
-addOrder('Minsk 139 Starovoitov.E.A. 2018.01.22');
-addOrder('St.Petersberg 222 Liberman.S.I. 2018.02.17');
+wh.removeElem('271','Volkov.A.V.');
 
-deleteOrder('222', 'Liberman.S.I.');
+wh.showElementByNumber('269','2018.03.21');
 
-showOrderByNumber('271', '2017.12.31');
+wh.showAllElements();
 
-showAllOrder();
