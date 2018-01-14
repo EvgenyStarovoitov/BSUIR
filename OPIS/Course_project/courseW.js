862b5c15c8448a88beba7e6cd9150f59f1e3ab8c
Evgeny <evgeny@MacBook-Pro-Evgeny.local>
2018-01-14 15:39:26 +0300

    refactoring in ES6

--- OPIS/Course_project/courseW.js
+++ OPIS/Course_project/courseW.js
@@ -1,198 +1,268 @@
-let mimi=0,
-    inputText = document.querySelector('.text_value'),
-    canvasElem = document.querySelector('.canvas'),
-    clearbtn = document.querySelector('.button_clear'),
-    AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext,
-    context = new AudioContext(),
-    ctx,
-    flag = false,
-    prevX = 0,
-    currX = 0,
-    prevY = 0,
-    currY = 0,
-    dot_flag = false,
-    x = "black",
-    y = 2,
-    widthElem = window.innerWidth,
-    heightElem = window.innerHeight;
+    let mimi=0,
+        inputText = document.querySelector('.text_value'),
+        canvasElem = document.querySelector('.canvas__row'),
+        clearbtn = document.querySelector('.button_clear'),
+        AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext,
+        context = new AudioContext(),
+        ctx,
+        flag = false,
+        prevX = 0,
+        currX = 0,
+        prevY = 0,
+        currY = 0,
+        dot_flag = false,
+        x = "black",
+        y = 2,
+        widthElem = window.innerWidth,
+        heightElem = window.innerHeight;
 
-class Sound {
-    constructor(gainAmount,history,log,speed,fadeTime,elm){
-        this.gainAmount = gainAmount;
-        this.history = history;
-        this.log = log;
-        this.speed = speed;
-        this.fadeTime = fadeTime;
-        // this.width = window.innerWidth;
-        // this.height = window.innerHeight;    
-        this.elm = elm;
-        this.elm.addEventListener('mousedown', this.onMouseDown.bind(this));
-        this.elm.addEventListener('mouseup', this.onMouseUp.bind(this));
-        this.elm.addEventListener('touchstart',this.onTouchStart.bind(this));
-        console.log("create a sound element");
-    };
-    onMouseDown(event){
-        console.log('md');
-        if (this.osc_) {
-          return;
-        }
-        this.updatePointer(event);
-        this.osc_ = this.createOscillator_();
-        this.lastFreq = this.getLastFrequency();
-        this.osc_.frequency.setValueAtTime(mimi, context.currentTime); 
-    }
-    onMouseUp(){
-        console.log('mu');
-        this.deleteOscillator_();
-        this.lastFreq = null;
-    }
-    onMouseMove(){
-        console.log('mm');
-        if (this.osc_) {
-          return;
-        }
-        this.updatePointer(event);
-        this.osc_ = this.createOscillator_();
-        this.lastFreq = this.getLastFrequency();
-        this.osc_.frequency.setValueAtTime(mimi, context.currentTime);  
-    }
-    onTouchStart(event) {
-        console.log('ts');
-        if (this.osc_) {
-          return;
-        }
-        this.osc_ = this.createOscillator_();
-        // this.lastFreq = this.getLastFrequency();
-        // this.osc_.frequency.value = this.lastFreq;
-        this.updatePage(event);
-    };
-    updatePage(e){
-        KeyFlag = false;
-        mimi = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-        gainNode.gain.value = (CurY/HEIGHT) * maxVol;
-    }
-    createOscillator_(){
-        let gain = context.createGain();
-        gain.value = 0;
-        gain.connect(context.destination);
-        this.gain_ = gain;
-        let osc = context.createOscillator();
-        osc.connect(gain);
-        gain.gain.linearRampToValueAtTime(this.gainAmount,
-            context.currentTime + this.fadeTime);
-        osc.start(0);
-        return osc;
-    };
-    deleteOscillator_(){
-        if (this.osc_) {
-            let endTime = context.currentTime + this.fadeTime;
-            this.gain_.gain.linearRampToValueAtTime(0, endTime);
-            this.osc_.stop(endTime);
-            this.osc_ = null;
-          };
-    };
-    getLastFrequency(){
-        let x = this.lastX;
-        let y = this.lastY;
-        let percent = 1 - (y / this.height);
-        let nyquist = context.sampleRate/2;
-        if (this.log) {
-        percent = this.logScale_(percent * 1000, 1000) / 1000;
+    class Sound {
+        constructor(gainAmount,history,log,speed,fadeTime,elm){
+            this.gainAmount = gainAmount;
+            this.history = history;
+            this.log = log;
+            this.speed = speed;
+            this.fadeTime = fadeTime;
+            this.elm = elm;
+            this.elm.addEventListener('mousedown', this.onMouseDown.bind(this));
+            this.elm.addEventListener('mouseup', this.onMouseUp.bind(this));
+            this.elm.addEventListener('mousemove',this.onMouseMove());
+            console.log("create a sound element");
         };
-        return percent * nyquist;
-    };
-    updatePointer(event){
-        console.log("update\n", event.type)
-        event = event || {};
-        let type = event.type || '';
-        if (type.indexOf('mouse') == 0) {
-          this.lastX = event.pageX;
-          this.lastY = event.pageY;
-        } else if (type.indexOf('touch') == 0) {
-          this.lastX = event.touches[0].pageX;
-          this.lastY = event.touches[0].pageY;
-        } else {
-          this.lastX = null;
-          this.lastY = null;
+        onMouseDown(event){
+            console.log('md');
+            if (this.osc_) {
+            return;
+            }
+            this.updatePointer(event);
+            this.osc_ = this.createOscillator_();
+            this.lastFreq = this.getLastFrequency();
+            this.osc_.frequency.setValueAtTime(mimi, context.currentTime); 
         }
+        onMouseUp(){
+            console.log('mu');
+            this.deleteOscillator_();
+            this.lastFreq = null;
+        }
+        onMouseMove(){
+            console.log('mm');
+            if (this.osc_) {
+            return;
+            }
+            this.osc_ = this.createOscillator_();
+            this.lastFreq = this.getLastFrequency();
+            this.osc_.frequency.setValueAtTime(mimi, context.currentTime);  
+            // this.updatePage()
+        }
+        createOscillator_(){
+            let gain = context.createGain();
+            gain.value = 0;
+            gain.connect(context.destination);
+            this.gain_ = gain;
+            let osc = context.createOscillator();
+            osc.connect(gain);
+            gain.gain.linearRampToValueAtTime(this.gainAmount,
+                context.currentTime + this.fadeTime);
+            osc.start(0);
+            return osc;
+        };
+        deleteOscillator_(){
+            if (this.osc_) {
+                let endTime = context.currentTime + this.fadeTime;
+                this.gain_.gain.linearRampToValueAtTime(0, endTime);
+                this.osc_.stop(endTime);
+                this.osc_ = null;
+            };
+        };
+        getLastFrequency(){
+            let x = this.lastX;
+            let y = this.lastY;
+            let percent = 1 - (y / this.height);
+            let nyquist = context.sampleRate/2;
+            if (this.log) {
+            percent = this.logScale_(percent * 1000, 1000) / 1000;
+            };
+            return percent * nyquist;
+        };
+        updatePointer(event){
+            console.log("update\n", event.type)
+            event = event || {};
+            let type = event.type || '';
+            if (type.indexOf('mouse') == 0) {
+            this.lastX = event.pageX;
+            this.lastY = event.pageY;
+            } else if (type.indexOf('touch') == 0) {
+            this.lastX = event.touches[0].pageX;
+            this.lastY = event.touches[0].pageY;
+            } else {
+            this.lastX = null;
+            this.lastY = null;
+            }
+        };
+        // updatePage(evenet){
+        //     this.getLastFrequency()
+        //     event = event || {};
+        //     this.lastX = event.pageX;
+        //     this.lastY = event.pageY;
+        //     currX = (window.Event) ? event.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+        
+        //     currY = (window.Event) ? event.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+            
+        //     this.osc_.frequency.value = (mimi/widthElem)
+        //     console.log(this.osc_.frequency.value,"\n",this.lastY,"\n",widthElem)
+        //     // gainNode.gain.value = (currY/heightElem) * maxVol;
+        // }
     };
-};
 
-let initCanvas = (widthCanvas, heightCanvas)=> {
-    ctx = canvasElem.getContext("2d");
-    canvasElem.width = widthElem;
-    canvasElem.height = heightElem;
-
-    console.log("init draw func")
-
-    canvasElem.addEventListener("mousemove", function (e) {
-        findxy('move', e)
-    }, false);
-    canvasElem.addEventListener("mousedown", function (e) {
-        findxy('down', e)
-    }, false);
-    canvasElem.addEventListener("mouseup", function (e) {
-        findxy('up', e)
-    }, false);
-    canvasElem.addEventListener("mouseout", function (e) {
-        findxy('out', e)
-    }, false);
-}
-
-let draw = () => {
-    ctx.beginPath();
-    ctx.moveTo(prevX, prevY);
-    ctx.lineTo(currX, currY);
-    ctx.strokeStyle = x;
-    ctx.lineWidth = y;
-    ctx.stroke();
-    ctx.closePath();
-}
-
-let clear = () => {
-    let m = confirm("Очистить холст?");
-    if (m) {
-        ctx.clearRect(0, 0, widthCanvas, heightCanvas);
-    }
-}
-
-let findxy = (res, e) => {
-    if (res == 'down') {
-        prevX = currX;
-        prevY = currY;
-        currX = e.clientX; 
-        currY = e.clientY;
-
-        flag = true;
-        dot_flag = true;
-        if (dot_flag) {
+    class CanvasEl {
+        constructor(width,height){
+            this.width = width;
+            this.height = height;
+            this.initCanvas();
+        }
+        initCanvas(){
+            ctx = canvasElem.getContext("2d");
+            canvasElem.width = this.width;
+            canvasElem.height = this.height;
+    
+            console.log("init draw func", canvasElem)
+    
+            canvasElem.addEventListener("mousemove", (e)=> {
+                this.findCoord('move', e)
+            }, false);
+            canvasElem.addEventListener("mousedown", (e)=> {
+                this.findCoord('down', e)
+            }, false);
+            canvasElem.addEventListener("mouseup", (e)=> {
+                this.findCoord('up', e)
+            }, false);
+            canvasElem.addEventListener("mouseout", (e)=> {
+                this.findCoord('out', e)
+            }, false);
+        };
+        draw(){
             ctx.beginPath();
-            ctx.fillStyle = x;
-            ctx.fillRect(currX, currY, 2, 2);
-            ctx.closePath();
-            dot_flag = false;
+            ctx.moveTo(prevX, prevY);
+            ctx.lineTo(currX, currY);
+            ctx.strokeStyle = x;
+            ctx.lineWidth = y;
+            ctx.stroke();
+            ctx.closePath();    
+        };
+        clear(){
+            let m = confirm("Очистить холст?");
+            if (m) {
+                ctx.clearRect(0, 0, this.width, this.height);
+            };
+        };
+        findCoord(res,e){
+            if (res == 'down') {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX; 
+                currY = e.clientY;
+                flag = true;
+                dot_flag = true;
+                if (dot_flag) {
+                    ctx.beginPath();
+                    ctx.fillStyle = x;
+                    ctx.fillRect(currX, currY, 2, 2);
+                    ctx.closePath();
+                    dot_flag = false;
+                }
+            }
+            if (res == 'up' || res == "out") {
+                flag = false;
+            }
+            if (res == 'move') {
+                if (flag) {
+                    prevX = currX;
+                    prevY = currY;
+                    currX = e.clientX;
+                    currY = e.clientY;
+                    this.draw();
+                }
+            }
         }
     }
-    if (res == 'up' || res == "out") {
-        flag = false;
-    }
-    if (res == 'move') {
-        if (flag) {
-            prevX = currX;
-            prevY = currY;
-            currX = e.clientX;
-            currY = e.clientY;
-            draw();
-        }
-    }
-}
 
-window.addEventListener('mousemove', ()=>{
-    mimi = event.pageY*20;
-    inputText.value = mimi;
-});
+    // let initCanvas = ()=> {
+    //     ctx = canvasElem.getContext("2d");
+    //     canvasElem.width = widthElem;
+    //     canvasElem.height = heightElem;
 
-clearbtn.addEventListener('click', clear )
+    //     console.log("init draw func")
 
-let some = new Sound(0.2,[],false,10,0.01,window);
-initCanvas(widthElem,heightElem);
+    //     canvasElem.addEventListener("mousemove", function (e) {
+    //         findxy('move', e)
+    //     }, false);
+    //     canvasElem.addEventListener("mousedown", function (e) {
+    //         findxy('down', e)
+    //     }, false);
+    //     canvasElem.addEventListener("mouseup", function (e) {
+    //         findxy('up', e)
+    //     }, false);
+    //     canvasElem.addEventListener("mouseout", function (e) {
+    //         findxy('out', e)
+    //     }, false);
+    // }
+
+    // let draw = () => {
+    //     ctx.beginPath();
+    //     ctx.moveTo(prevX, prevY);
+    //     ctx.lineTo(currX, currY);
+    //     ctx.strokeStyle = x;
+    //     ctx.lineWidth = y;
+    //     ctx.stroke();
+    //     ctx.closePath();
+    // }
+
+    // let clear = () => {
+    //     let m = confirm("Очистить холст?");
+    //     if (m) {
+    //         ctx.clearRect(0, 0, widthElem, heightElem);
+    //     }
+    // }
+
+    // let findxy = (res, e) => {
+    //     if (res == 'down') {
+    //         prevX = currX;
+    //         prevY = currY;
+    //         currX = e.clientX; 
+    //         currY = e.clientY;
+    //         flag = true;
+    //         dot_flag = true;
+    //         if (dot_flag) {
+    //             ctx.beginPath();
+    //             ctx.fillStyle = x;
+    //             ctx.fillRect(currX, currY, 2, 2);
+    //             ctx.closePath();
+    //             dot_flag = false;
+    //         }
+    //     }
+    //     if (res == 'up' || res == "out") {
+    //         flag = false;
+    //     }
+    //     if (res == 'move') {
+    //         if (flag) {
+    //             prevX = currX;
+    //             prevY = currY;
+    //             currX = e.clientX;
+    //             currY = e.clientY;
+    //             draw();
+    //         }
+    //     }
+    // }
+
+    window.addEventListener('mousemove', ()=>{
+        mimi = event.pageY*20;
+        inputText.value = mimi;
+    });
+
+    // clearbtn.addEventListener('click', clear )
+
+    let some = new Sound(0.2,[],false,10,0.01,window);
+    let newCanvas = new CanvasEl(widthElem,heightElem);
+
+    clearbtn.addEventListener('click', newCanvas.clear )
+    // initCanvas();
--- OPIS/Course_project/hardcode.js
+++ OPIS/Course_project/hardcode.js
@@ -1,77 +1,6 @@
-let oscillator = audioCtx.createOscillator();
-oscillator.type = 'square';
-oscillator.connect(audioCtx.destination);
-// oscillator.start();
 
-let startSound = ()=>{
-    let oscillator = audioCtx.createOscillator();
-    oscillator.frequency.setValueAtTime(height, audioCtx.currentTime); 
-    oscillator.start(); 
-}
-let endSound = ()=>{
-    oscillator.frequency.setValueAtTime(height, audioCtx.currentTime); 
-    oscillator.deleteOscillator_()
-    oscillator.stop();
-}
-
-window.addEventListener('mousedown', startSound, false);
-window.addEventListener('mouseup', endSound, false);
-
-let height = document.documentElement.clientHeight
-console.log(height);
-let context = new AudioContext();
-
-let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-let height = document.documentElement.clientHeight;
-
-
-    // draw(){
-    //     var canvasCtx = canvasElem.getContext('2d');
-    //     canvasElem.width = this.width;
-    //     canvasElem.height = this.height;
-    //     let KeyFlag = true;
-    //     var KeyX = 1;
-    //     var KeyY = 0.01
-    //     let rX = 0;
-    //     let rY = 0;
-    //     let size = 6;
-    //     if(KeyFlag == true) {
-    //         rX = KeyX;
-    //         rY = KeyY;
-    //       } else {
-    //         rX = CurX;
-    //         rY = CurY;
-    //       }
-    //       let rC = 10;
-          
-    //     canvasCtx.globalAlpha = 0.2;
-    //     canvasCtx.fillStyle = "#d6cdcd";
-    //     canvasCtx.strokeStyle = "#d6cdcd";
-    //     canvasCtx.beginPath();
-    //     canvasCtx.font = '20px Inconsolata';
-    //     canvasCtx.fillRect(10 - size/2, 20 - size/2, size, size);
-    //     canvasCtx.fill();
-    //     canvasCtx.closePath(); 
-    //     // if (this.osc_) {
-    //     //     canvasCtx.beginPath();
-    //     //     canvasCtx.font = '20px Inconsolata';
-    //     //     canvasCtx.fillStyle = "#d6cdcd";
-    //     //     canvasCtx.fillRect(10 - size/2, 20 - size/2, size, size);
-    //     //     canvasCtx.fill();
-    //     //     canvasCtx.closePath(); 
-    //     //   }
-    //     //   console.log("i'm draw!!!!!",canvasCtx,this.osc_)
-    //     // for(let i=1;i<=15;i=i+2) {
-    //     //     canvasCtx.beginPath();
-    //     //     canvasCtx.fillStyle = 'rgb(' + 100+(i*10);
-    //     //     canvasCtx.arc(1,rC/2+i,(Math.PI/180)*0,(Math.PI/180)*360,false);
-    //     //     canvasCtx.fill();
-    //     //     canvasCtx.closePath();     
-    //     // } 
-    //     requestAnimationFrame(this.draw.bind(this));
-    // };
-
-
+document.onmousemove = updatePage;
+function updatePage(e) {
     // create web audio api context
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioCtx = new AudioContext();
@@ -96,17 +25,273 @@ oscillator.onended = function() {
 }
 gainNode.gain.value = initialVol;
 // Mouse pointer coordinates
-var CurX;
-var CurY;
+var currX;
+var currY;
 // Get new mouse pointer coordinates when mouse is moved
 // then set new gain and pitch values
-document.onmousemove = updatePage;
-function updatePage(e) {
-    KeyFlag = false;
-    CurX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
-    CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+
+    currX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+
+    currY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
     
-    oscillator.frequency.value = (CurX/widthElem) * maxFreq;
-    gainNode.gain.value = (CurY/heightElem) * maxVol;
-    canvasDraw();
+    oscillator.frequency.value = (currX/widthElem) * maxFreq;
+    gainNode.gain.value = (currY/heightElem) * maxVol;
 }
+
+
+Polymer('g-spectrogram', {
+    // Show the controls UI.
+    controls: false,
+    // Log mode.
+    log: false,
+    // Show axis labels, and how many ticks.
+    labels: false,
+    ticks: 5,
+    speed: 2,
+    // FFT bin size,
+    fftsize: 2048,
+    oscillator: false,
+    color: false,
+  
+    attachedCallback: function() {
+      this.tempCanvas = document.createElement('canvas'),
+      console.log('Created spectrogram');
+      // Get input from the microphone.
+      if (navigator.mozGetUserMedia) {
+        navigator.mozGetUserMedia({audio: true},
+                                  this.onStream.bind(this),
+                                  this.onStreamError.bind(this));
+      } else if (navigator.webkitGetUserMedia) {
+        navigator.webkitGetUserMedia({audio: true},
+                                  this.onStream.bind(this),
+                                  this.onStreamError.bind(this));
+      }
+      this.ctx = this.$.canvas.getContext('2d');
+    },
+  
+    render: function() {
+      //console.log('Render');
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+  
+      var didResize = false;
+      // Ensure dimensions are accurate.
+      if (this.$.canvas.width != this.width) {
+        this.$.canvas.width = this.width;
+        this.$.labels.width = this.width;
+        didResize = true;
+      }
+      if (this.$.canvas.height != this.height) {
+        this.$.canvas.height = this.height;
+        this.$.labels.height = this.height;
+        didResize = true;
+      }
+  
+      //this.renderTimeDomain();
+      this.renderFreqDomain();
+  
+      if (this.labels && didResize) {
+        this.renderAxesLabels();
+      }
+  
+      requestAnimationFrame(this.render.bind(this));
+  
+      var now = new Date();
+      if (this.lastRenderTime_) {
+        this.instantaneousFPS = now - this.lastRenderTime_;
+      }
+      this.lastRenderTime_ = now;
+    },
+  
+    renderTimeDomain: function() {
+      var times = new Uint8Array(this.analyser.frequencyBinCount);
+      this.analyser.getByteTimeDomainData(times);
+  
+      for (var i = 0; i < times.length; i++) {
+        var value = times[i];
+        var percent = value / 256;
+        var barHeight = this.height * percent;
+        var offset = this.height - barHeight - 1;
+        var barWidth = this.width/times.length;
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillRect(i * barWidth, offset, 1, 1);
+      }
+    },
+  
+    renderFreqDomain: function() {
+      var freq = new Uint8Array(this.analyser.frequencyBinCount);
+      this.analyser.getByteFrequencyData(freq);
+  
+      var ctx = this.ctx;
+      // Copy the current canvas onto the temp canvas.
+      this.tempCanvas.width = this.width;
+      this.tempCanvas.height = this.height;
+      //console.log(this.$.canvas.height, this.tempCanvas.height);
+      var tempCtx = this.tempCanvas.getContext('2d');
+      tempCtx.drawImage(this.$.canvas, 0, 0, this.width, this.height);
+  
+      // Iterate over the frequencies.
+      for (var i = 0; i < freq.length; i++) {
+        var value;
+        // Draw each pixel with the specific color.
+        if (this.log) {
+          logIndex = this.logScale(i, freq.length);
+          value = freq[logIndex];
+        } else {
+          value = freq[i];
+        }
+  
+        ctx.fillStyle = (this.color ? this.getFullColor(value) : this.getGrayColor(value));
+  
+        var percent = i / freq.length;
+        var y = Math.round(percent * this.height);
+  
+        // draw the line at the right side of the canvas
+        ctx.fillRect(this.width - this.speed, this.height - y,
+                     this.speed, this.speed);
+      }
+  
+      // Translate the canvas.
+      ctx.translate(-this.speed, 0);
+      // Draw the copied image.
+      ctx.drawImage(this.tempCanvas, 0, 0, this.width, this.height,
+                    0, 0, this.width, this.height);
+  
+      // Reset the transformation matrix.
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+    },
+  
+    /**
+     * Given an index and the total number of entries, return the
+     * log-scaled value.
+     */
+    logScale: function(index, total, opt_base) {
+      var base = opt_base || 2;
+      var logmax = this.logBase(total + 1, base);
+      var exp = logmax * index / total;
+      return Math.round(Math.pow(base, exp) - 1);
+    },
+  
+    logBase: function(val, base) {
+      return Math.log(val) / Math.log(base);
+    },
+  
+    renderAxesLabels: function() {
+      var canvas = this.$.labels;
+      canvas.width = this.width;
+      canvas.height = this.height;
+      var ctx = canvas.getContext('2d');
+      var startFreq = 440;
+      var nyquist = context.sampleRate/2;
+      var endFreq = nyquist - startFreq;
+      var step = (endFreq - startFreq) / this.ticks;
+      var yLabelOffset = 5;
+      // Render the vertical frequency axis.
+      for (var i = 0; i <= this.ticks; i++) {
+        var freq = startFreq + (step * i);
+        // Get the y coordinate from the current label.
+        var index = this.freqToIndex(freq);
+        var percent = index / this.getFFTBinCount();
+        var y = (1-percent) * this.height;
+        var x = this.width - 60;
+        // Get the value for the current y coordinate.
+        var label;
+        if (this.log) {
+          // Handle a logarithmic scale.
+          var logIndex = this.logScale(index, this.getFFTBinCount());
+          // Never show 0 Hz.
+          freq = Math.max(1, this.indexToFreq(logIndex));
+        }
+        var label = this.formatFreq(freq);
+        var units = this.formatUnits(freq);
+        ctx.font = '16px Inconsolata';
+        // Draw the value.
+        ctx.textAlign = 'right';
+        ctx.fillText(label, x, y + yLabelOffset);
+        // Draw the units.
+        ctx.textAlign = 'left';
+        ctx.fillText(units, x + 10, y + yLabelOffset);
+        // Draw a tick mark.
+        ctx.fillRect(x + 40, y, 30, 2);
+      }
+    },
+  
+    clearAxesLabels: function() {
+      var canvas = this.$.labels;
+      var ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, this.width, this.height);
+    },
+  
+    formatFreq: function(freq) {
+      return (freq >= 1000 ? (freq/1000).toFixed(1) : Math.round(freq));
+    },
+  
+    formatUnits: function(freq) {
+      return (freq >= 1000 ? 'KHz' : 'Hz');
+    },
+  
+    indexToFreq: function(index) {
+      var nyquist = context.sampleRate/2;
+      return nyquist/this.getFFTBinCount() * index;
+    },
+  
+    freqToIndex: function(frequency) {
+      var nyquist = context.sampleRate/2;
+      return Math.round(frequency/nyquist * this.getFFTBinCount());
+    },
+  
+    getFFTBinCount: function() {
+      return this.fftsize / 2;
+    },
+  
+    onStream: function(stream) {
+      var input = context.createMediaStreamSource(stream);
+      var analyser = context.createAnalyser();
+      analyser.smoothingTimeConstant = 0;
+      analyser.fftSize = this.fftsize;
+  
+      // Connect graph.
+      input.connect(analyser);
+  
+      this.analyser = analyser;
+      // Setup a timer to visualize some stuff.
+      this.render();
+    },
+  
+    onStreamError: function(e) {
+      console.error(e);
+    },
+  
+    getGrayColor: function(value) {
+      return 'rgb(V, V, V)'.replace(/V/g, 255 - value);
+    },
+  
+    getFullColor: function(value) {
+      var fromH = 62;
+      var toH = 0;
+      var percent = value / 255;
+      var delta = percent * (toH - fromH);
+      var hue = fromH + delta;
+      return 'hsl(H, 100%, 50%)'.replace(/H/g, hue);
+    },
+    
+    logChanged: function() {
+      if (this.labels) {
+        this.renderAxesLabels();
+      }
+    },
+  
+    ticksChanged: function() {
+      if (this.labels) {
+        this.renderAxesLabels();
+      }
+    },
+  
+    labelsChanged: function() {
+      if (this.labels) {
+        this.renderAxesLabels();
+      } else {
+        this.clearAxesLabels();
+      }
+    }
+  });
